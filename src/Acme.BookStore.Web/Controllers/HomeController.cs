819b356540e028c11927b5eb5895f9271fec9fc7
Nicolas <nicolusmaluleke@gmail.com>
2023-10-14 14:32:47 +0200

    initial commit

---
+++ src/Acme.BookStore.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.BookStore.Web;
+
+[Route("/")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
